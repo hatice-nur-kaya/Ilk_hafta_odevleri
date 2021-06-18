86d26e35319aabc9baa4d4bdfb8e87ded68d1f62
haticenur.4455 <haticenur.4455@gmail.com>
2021-06-18 08:47:49 +0300

    deneme
    taksimetre

---
+++ src/Dairenin_Alanını_Çevresini_Bulan_Program/Main.java
@@ -0,0 +1,10 @@
+package Dairenin_Alanýný_Çevresini_Bulan_Program;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/TaksimetreProgramı/Main.java
+++ src/TaksimetreProgramı/Main.java
@@ -1,10 +1,17 @@
 package TaksimetreProgramý;
 
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		Scanner info =new Scanner(System.in);
+		int km;
+		System.out.println("Kilometre Giriniz :");
+		double taksimetre=2.20,total,startPrice=10;
+		km=info.nextInt();
+		total =(km*taksimetre);
+		total=startPrice;
+		total=(total<20)?20:total;
+		System.out.print("Toplam Tutar : "+total);
+		}
 }
