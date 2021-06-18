5cbb478e06f558fff6abb9d3509f9bec4964eddf
haticenur.4455 <haticenur.4455@gmail.com>
2021-06-18 09:31:48 +0300

    güncelleme

--- src/Vucut_Kitle_Indeksi_Hesaplayan_Program/Main.java
+++ src/Vucut_Kitle_Indeksi_Hesaplayan_Program/Main.java
@@ -1,10 +1,18 @@
 package Vucut_Kitle_Indeksi_Hesaplayan_Program;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		double boy ;
+		double kilo ;
+		Scanner info = new Scanner(System.in);
+		System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz : ");
+		boy=info.nextDouble();
+		System.out.print("Lütfen kilonuzu giriniz : ");
+		kilo=info.nextDouble();
+		double index =(kilo / (boy * boy)) ;
+		System.out.print("Vücut Kitle Ýndeksiniz : " + index);
 	}
-
 }
