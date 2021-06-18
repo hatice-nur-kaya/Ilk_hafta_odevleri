258051ef9849ff3e2c3ee11a93dedf9ad276f6b3
haticenur.4455 <haticenur.4455@gmail.com>
2021-06-18 09:01:22 +0300

    güncelleme
    Dairenin alanı ve çevresi

--- src/Dairenin_Alanını_Çevresini_Bulan_Program/Main.java
+++ src/Dairenin_Alanını_Çevresini_Bulan_Program/Main.java
@@ -1,9 +1,19 @@
 package Dairenin_Alanýný_Çevresini_Bulan_Program;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner info =new Scanner(System.in);
+		System.out.print("Yarýçap Giriniz : ");
+		double pi=3.34;
+		int r;
+		r=info.nextInt();
+		double alan = (r*r*pi);
+		double cevre =(2*pi*r);
+		System.out.println("Dairenin Alaný : "+ alan);
+		System.out.print("Dairenin Çevresi : "+ cevre);
 
 	}
 
