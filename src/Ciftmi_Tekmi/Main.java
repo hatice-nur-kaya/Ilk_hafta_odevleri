193402873361ebf98aebb846baff609fbd053099
haticenur.4455 <haticenur.4455@gmail.com>
2021-06-20 09:17:07 +0300

    Ucak Bileti

---
+++ src/Ciftmi_Tekmi/Main.java
@@ -0,0 +1,9 @@
+package Ciftmi_Tekmi;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- src/Ucak_Bileti/Main.java
+++ src/Ucak_Bileti/Main.java
@@ -6,32 +6,43 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Scanner info = new Scanner(System.in);
-		int yas,yolculukTipi;
-		System.out.println("Mesafe Giriniz : ");
-		double km=info.nextDouble() ;
-		double ucret = km * 0.10;
-		System.out.println("Yaþýnýz: ");
-	    yas = info.nextInt();
-	    System.out.println("1-Toplama\n 2-Çýkarma\n 3-Bölme\n 4-Çarpma");
-		int select =info.nextInt();
-		
-	    System.out.println("Yolculuk Tipini Giriniz => (1 => Tek Yön , 2 => Gidiþ Dönüþ ): ");
-	    switch (select) {
-	    case 1: {
-	    //	 System.out.println(yolculukTipi+" Tek Yön");
-	    	break;
-	    }
-	    case 2: {
-	    	// System.out.println(yolculukTipi+ "Çift Yön ");
-	    	break;
-	    }
-		//System.out.println("Normal Tutar : "+ucret);
-		//if(yas <12 ) {
-			
-		//}
+		Scanner info =new Scanner(System.in);
+		int km,yas,yolcuTipi;
+		double normalFiyat,yasIndirimi,tipIndirimi;
+		System.out.println("Mesafeyi Griniz : ");
+		km=info.nextInt();
+		System.out.println("Yaþýnýzý Griniz : ");
+		yas=info.nextInt();
+		System.out.println("Yolculuk Tipini  Griniz : ");
+		yolcuTipi=info.nextInt();
+		if(km>0 &&yas>0 && (yolcuTipi==1|| yolcuTipi==2)) {
+			normalFiyat=km*0.10;
+			if(yas<12) {
+				yasIndirimi =normalFiyat*0.5;
+			}
+			else if (yas>=10 &&yas<=24) {
+				yasIndirimi=normalFiyat*0.10;
+			}
+			else if (yas> 65) {
+				yasIndirimi=normalFiyat*0.30;
+			}
+			else {
+				yasIndirimi =0;
+			}
+			normalFiyat -=yasIndirimi;
+			if(yolcuTipi==2) {
+				tipIndirimi =normalFiyat*0.20;
+				normalFiyat =(normalFiyat- tipIndirimi);
+			}
+			System.out.println("Bilet Tutarý : "+normalFiyat +"TL");
+		}
+		else {
+			System.out.println("Girdiðiniz Deðerler Eksik veya Yanlýþ!");
+
+		}
+      }
 	 
 	}
-	}
-}
+
+
 
