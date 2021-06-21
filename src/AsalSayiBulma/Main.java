bc83377628855411e2a47d05f862ad7438060dcb
haticenur.4455 <haticenur.4455@gmail.com>
2021-06-21 20:13:25 +0300

    Güncelleme

---
+++ src/AsalSayiBulma/Main.java
@@ -0,0 +1,17 @@
+package AsalSayiBulma;
+
+public class Main {
+
+	public static void main(String[] args) {
+		 int sayi;
+         for(int i=2; i<=100; i++){
+             sayi=0;
+             for(int j=2; j<=i/2; j++){
+                 if(i%j==0) sayi++;
+             }
+             if(sayi==0) System.out.println(+i);
+         }
+     }
+ }
+
+
