a29d4f3123bcd8b79c647861b72abe67d80fe3e1
haticenur.4455 <haticenur.4455@gmail.com>
2021-06-21 19:52:12 +0300

    Güncelleme

---
+++ src/TersUcgen/Main.java
@@ -0,0 +1,27 @@
+package TersUcgen;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+        Scanner klavye=new Scanner(System.in);
+        
+        System.out.println("Bir Sayý Giriniz");
+        int n=klavye.nextInt();
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(" ");
+            }   
+            for(int k=2*n-1;k>=(2*i+1);k--){
+                System.out.print("*");
+                
+            }
+                System.out.println();   
+
+        }
+	}
+
+}
