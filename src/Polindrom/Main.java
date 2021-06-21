aa692deb3b3859f68d59f0a7d5b993544a48f926
haticenur.4455 <haticenur.4455@gmail.com>
2021-06-21 20:49:42 +0300

    Güncelleme

---
+++ src/Polindrom/Main.java
@@ -0,0 +1,29 @@
+package Polindrom;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		 Scanner scan = new Scanner(System.in);
+	        System.out.println("Sayi Giriniz : ");
+	        int palindrom = scan.nextInt();
+	        int ilkdeger = palindrom;        
+	        int length = String.valueOf(palindrom).length();
+	         
+	        int kalan,deger=0;
+	        for (int i = 0; i<length; i++){
+	             
+	            kalan = palindrom % 10;
+	            deger = deger * 10 + kalan;
+	            palindrom = palindrom /10;
+	        }
+	         
+	        if(deger == ilkdeger){
+	            System.out.println(deger + " Palindrom Sayidir.");
+	        }else{
+	            System.out.println(deger + " Palindrom Sayi Degildir.");
+	        }
+	         
+	    }
+	}
