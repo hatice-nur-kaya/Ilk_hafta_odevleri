fc4f54bbb18591fd9a0e15f3ef1a777e8f17aa79
haticenur.4455 <haticenur.4455@gmail.com>
2021-06-21 19:48:50 +0300

    Güncelleme

---
+++ src/MukemmelSayi/Main.java
@@ -0,0 +1,28 @@
+package MukemmelSayi;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner info =new Scanner(System.in);
+int toplam = 0;
+        
+        System.out.print("Bir sayi giriniz: ");
+        int sayi = info.nextInt();
+        for(int i = 1; i < sayi; i++)
+        {
+            if(sayi % i == 0) {
+                toplam += i;
+            }
+        }
+        if(sayi == toplam) {
+            System.out.println(sayi + " Mükemmel bir sayidir");
+        }
+        else {
+            System.out.println(sayi + " Mükemmel bir sayi degildir.");
+        }
+	}
+
+}
---
+++ src/SayilarinMinMaxDegeri/Main.java
@@ -0,0 +1,17 @@
+package SayilarinMinMaxDegeri;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner info =new Scanner(System.in);
+		for(int i=0;i<5;i++){ 
+	        System.out.println("BÝr Sayý girin: "); 
+	        i=info.nextInt();
+	    }
+		
+	}
+
+}
