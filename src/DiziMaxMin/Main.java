585929c0fb2e79618a4f1c7a8e2fbea69c082056
haticenur.4455 <haticenur.4455@gmail.com>
2021-06-25 23:39:55 +0300

    Güncelleme

---
+++ src/DiziMaxMin/Main.java
@@ -0,0 +1,31 @@
+package DiziMaxMin;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		  int[] list = {56, 34, 1, 8, 101, -2, -33};
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("Dizi : " + Arrays.toString(list));
+	        System.out.print("Girilen Sayi : ");
+	        int sayi = sc.nextInt();
+	        int min = list[0];
+	        int max = list[0];
+
+	        for (int i : list) {
+	            if (i < sayi &&  (sayi - i < 0 ? (sayi - i) * -1 : sayi -i ) < (sayi - min < 0 ? (sayi - min) * -1 : sayi -min )) {
+	                min = i;
+	            }
+	            if (i > sayi &&  (sayi - i < 0 ? (sayi - i) * -1 : sayi -i ) < (sayi - min < 0 ? (sayi - min) * -1 : sayi -min )) {
+	                max = i;
+	            }
+	        }
+
+	        System.out.println("Girilen sayıdan küçük en yakın sayı : " + min);
+	        System.out.println("Girilen sayıdan büyük en yakın sayı : " + max);
+	    }
+
+	}
+
