a32f33e92e903f5993ba22a313406193c35696d8
haticenur.4455 <haticenur.4455@gmail.com>
2021-06-27 19:33:33 +0300

    Güncelle

---
+++ src/DiziFrekansi/Main.java
@@ -0,0 +1,29 @@
+package DiziFrekansi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		  
+		 int [] arr = new int [] {10, 20, 20, 10, 10, 20, 5, 20};  
+	        
+	        int [] fr = new int [arr.length];  
+	        int visited = -1;  
+	        for(int i = 0; i < arr.length; i++){  
+	            int count = 1;  
+	            for(int j = i+1; j < arr.length; j++){  
+	                if(arr[i] == arr[j]){  
+	                    count++;  
+	                
+	                    fr[j] = visited;  
+	                }  
+	            }  
+	            if(fr[i] != visited)  
+	                fr[i] = count;  
+	        }  
+	   
+	        System.out.println(" Tekrar Sayıları");  
+	        for(int i = 0; i < fr.length; i++){  
+	            if(fr[i] != visited)  
+	                System.out.println(arr[i] +" Sayısı "+ fr[i]+" kere tekrar edildi ");  
+	        }  
+	    }} 
