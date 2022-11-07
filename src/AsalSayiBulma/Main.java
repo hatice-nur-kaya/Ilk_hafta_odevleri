e4a60b63b7c970e685522ae6027e92b6a7640b26
Hatice Nur KAYA <66225075+HNK-hub@users.noreply.github.com>
2022-11-07 14:03:36 +0300

    Update Main.java

--- src/AsalSayiBulma/Main.java
+++ src/AsalSayiBulma/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		 int sayi;
-         for(int i=2; i<=100; i++){
+         for(int i=2; i<=100; i++){ //for döngüsü ile sayıları dolaşarak asalları tespit ederiz.
              sayi=0;
              for(int j=2; j<=i/2; j++){
                  if(i%j==0) sayi++;
